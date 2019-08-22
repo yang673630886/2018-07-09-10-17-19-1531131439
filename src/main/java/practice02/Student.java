1ba4725fb438c472803659d976f332510455e935
yangjingsheng <673630886@qq.com>
2019-08-22 21:43:38 +0800

    2

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,21 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+	
+	private Integer klass;
+
+	public Student(String name, int age,Integer klass) {
+		super(name, age);
+		this.klass = klass;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getKlass() {
+		return this.klass;
+	}
+	
+	public String introduce() {
+		return "I am a Student. I am at Class "+this.klass+".";
+		
+	}
 }
